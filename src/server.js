07a76c7f7729817cd8cd6503d4e8aa13872e9022
Lucas Natanael <naillathdev@gmail.com>
2026-09-20 19:32:30 -0300

    feat: implementar rotas GET e POST de aventureiros

--- src/server.js
+++ src/server.js
@@ -3,8 +3,37 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const aventureiros = [];
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('⚔️ Guilda API online. Bem-vindo à guilda aventureiro(a)!');
+});
+
+app.get('/aventureiros', (req, res) => {
+    res.json(aventureiros);
+});
+
+app.get('/aventureiros/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const aventureiro = aventureiros.find(aventureiro => aventureiro.id === id);
+
+    if(!aventureiro) {
+        return res.status(404).json({
+            mensagem: "Aventureiro não encontrado"
+        });
+    }
+
+    res.json(aventureiro);
+});
+
+app.post('/aventureiros', (req, res) => {
+    const aventureiro = req.body;
+
+    aventureiros.push(aventureiro);
+
+    res.json(aventureiro);
 });
 
 app.listen(port, () => {
